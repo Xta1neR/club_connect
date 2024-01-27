256f10ff9e1302b738e1221e2fe030c2531b34b3
Rituraj Goswami <goswami.rituraj03@gmail.com>
2024-01-27 11:02:29 +0530

    Restructed the express js module and the servers so that the logics can be easily edited accordingly. Also added the auth-router and auth-controller folder in the directory structure.

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {signup, signin} = require("../controllers/userControllers");
-const userRouter = express.Router();
-
-userRouter.post("/signup", signup);
-
-userRouter.post("/signin", signin);
-
-module.exports = userRouter;
