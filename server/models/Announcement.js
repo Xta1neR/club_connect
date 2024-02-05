493708742fa3ac983fa7efda0c70488337cf725b
Rituraj Goswami <goswami.rituraj03@gmail.com>
2024-02-05 23:28:46 +0530

    Added the logic on how to add, edit or delete an announcement in website

---
+++ server/models/Announcement.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Announcement = mongoose.model('Announcement', announcementSchema);
+
+module.exports = Announcement;
